efaee534e1d84e270b17b9cbb8ba57f52abb69b2
Iheme Uchenna Howell <38653437+ihemehowell@users.noreply.github.com>
2025-08-21 09:30:13 +0100

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,20 +1,25 @@
 import React from 'react'
 import HeroSection from '../components/HeroSection'
+import CategoryHighlights from '../components/CategoryHighlights'
 import FeaturedProducts from '../components/FeaturedProducts'
 import ProductShowcase from '../components/ProductShowcase'
+import PromoBanner from '../components/PromoBanner'
 import Testimonials from '../components/Testimonial'
 import Newsletter from '../components/Newsletter'
+import Footer from '../components/Footer'
 import WithSpinner from '../components/WithSpinner';
 
 const Home = () => {
   return (
     <div>
       <HeroSection/>
+      <CategoryHighlights />   
       <FeaturedProducts />
       <ProductShowcase />
+      <PromoBanner />          
       <Testimonials />
       <Newsletter />
+      <Footer />               
     </div>
   )
 }
-export default WithSpinner(Home);
