2755c6e68d5a9de864dc530070a3fd8177cc9ca1
Iheme Uchenna Howell <38653437+ihemehowell@users.noreply.github.com>
2025-08-21 14:10:45 +0100

    Update Shop.jsx

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -142,7 +142,7 @@ function Shop({}) {
           <div
             className={view === "grid"
               ? "grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8"
-              : "flex  gap-6"}
+              : "flex mr-4 gap-4"}
           >
             {filteredProducts.map((product) => (
               <ProductCard key={product.id} product={product} view={view} />
