06a1602f0b7a0de9562123f954345ee9aee5764c
Iheme Uchenna Howell <38653437+ihemehowell@users.noreply.github.com>
2025-08-21 11:48:09 +0100

    Update CategoryHighlights.jsx

--- src/components/CategoryHighlights.jsx
+++ src/components/CategoryHighlights.jsx
@@ -4,7 +4,7 @@ import { Baby, Shirt, ShoppingBag,Footprints } from "lucide-react";
 const categories = [
   { id: 1, name: "Boys", icon: <Shirt className="w-8 h-8 text-blue-500" />, bg: "bg-blue-100" },
   { id: 2, name: "Girls", icon: <Shirt className="w-8 h-8 text-blue-500" />, bg: "bg-pink-100" },
-  { id: 3, name: "Girls", icon: <Footprint className="w-8 h-8 text-blue-500" />, bg: "bg-gray-100" },
+  { id: 3, name: "Shoes", icon: <Footprint className="w-8 h-8 text-blue-500" />, bg: "bg-gray-100" },
   { id: 4, name: "Babies", icon: <Baby className="w-8 h-8 text-purple-500" />, bg: "bg-purple-100" },
   { id: 5, name: "Accessories", icon: <ShoppingBag className="w-8 h-8 text-green-500" />, bg: "bg-green-100" },
 ];
